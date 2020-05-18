a01fbc5857909b3b1028b50336434e3a2ac992db
kvazimode <kvazimode2@gmail.com>
2020-05-18 21:23:52 +0300

    comment mocks

---
+++ src/mocks/comment.js
@@ -0,0 +1,24 @@
+export default {
+  id: 0,
+  author: [
+    `Joe`,
+    `Dominator3000`,
+    `Foxy`,
+    `1231`,
+    `Pony Expert`,
+    `Lidya`,
+    `Roxy`,
+    `Leo Tolstoy`,
+  ],
+  comment: [
+    `Fantastic story! I'm impressed!`,
+    `A have seen this actor in another movie. He was better in this one.`,
+    `Nothing special. Not gonna watch it again in a short time.`,
+    `My wife has laughed out loud. Was shame about her in cinema. I kinda liked the movie too`,
+    `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+    `I like Tarantino films and wasn't expecting this story to touch me that much. Cried lika a girl!`,
+    `Let me handshake operator of this film! What an angles! Color gamma! Scene planning! Wow!`,
+  ],
+  date: ``,
+  emotion: [`smile`, `sleeping`, `puke`, `angry`]
+};
